39418bdccf5430dab518efb0d9741106f6362cc1
cavendishsama <alibabaloo.aut@gmail.com>
2023-04-04 16:46:39 +0430

    sysinfo test is added

---
+++ user/sysinfotest.c
@@ -0,0 +1,15 @@
+#include "./kernel/types.h"
+#include "./kernel/stat.h"
+#include "./user/user.h"
+#include "./kernel/sysinfo.h"
+
+int main(int argc, char **argv){
+    struct sysinfo info;
+    sysinfo(&info);
+    printf("uptime = %d\n", info.uptime);
+    printf("Total Ram = %d\n", info.totalram);
+    printf("Free Ram = %d\n", info.freeram);
+    printf("Procs = %d\n", info.procs);
+    printf("successful\n");
+    exit(0);
+}
