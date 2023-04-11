dcc62479bbabf571b72d146b3398bad93186e001
cavendishsama <alibabaloo.aut@gmail.com>
2023-04-11 15:56:48 +0430

    test file is modified

--- user/sysinfotest.c
+++ user/sysinfotest.c
@@ -6,7 +6,8 @@
 int main(int argc, char **argv){
     struct sysinfo info;
     sysinfo(&info);
-    printf("uptime = %d\n", info.uptime);
+    printf("uptime = %d ticks\n", info.uptime);
+    // printf("uptime approximately ~= %.1fseconds\n", info.uptime * 0.1);
     printf("Total Ram = %d\n", info.totalram);
     printf("Free Ram = %d\n", info.freeram);
     printf("Procs = %d\n", info.procs);
